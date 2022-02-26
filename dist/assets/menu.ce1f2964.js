f7dc58e40b6b328daec2c87505af1248cc8465d9
Christian M. Schrader <cmschrader@wpi.edu>
2022-02-25 20:39:09 -0500

    Revert "Change of dist folder"
    This reverts commit 36198ec7085eea4c7a76bc634e7b3f07000e6aca.

---
+++ dist/assets/menu.ce1f2964.js
@@ -0,0 +1,1 @@
+var e=document.getElementById("burger-menu"),t=document.getElementById("menu-links");e.addEventListener("click",function(){this.classList.toggle("close"),t.classList.toggle("overlay")});
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'rollup'
 
 const root = resolve(__dirname, "src")
-const outDir =  resolve(__dirname, "docs")
+const outDir =  resolve(__dirname, "dist")
 
 export default defineConfig({
     root,
